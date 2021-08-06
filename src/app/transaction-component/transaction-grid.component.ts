ba82361788ce14f00286a375850f6a328cbfae40
dobromirts <dobromir.ts@gmail.com>
2021-08-06 13:12:55 +0300

    chore(*): remove deprecated injection token

--- src/app/transaction-component/transaction-grid.component.ts
+++ src/app/transaction-component/transaction-grid.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { IgxGridTransaction, IgxTransactionService } from 'igniteui-angular';
 
 @Component({
-    providers: [/* Injection token 'IgxGridTransaction' has been deprecated. Please refer to the update guide for more details. */
-/* Injection token 'IgxGridTransaction' has been deprecated. Please refer to the update guide for more details. */
-{ provide: IgxGridTransaction, useClass: IgxTransactionService }],
     selector: 'app-grid-transactions',
     template: '<ng-content></ng-content>'
 })
